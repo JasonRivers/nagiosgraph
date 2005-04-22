076ec600bdeabf7cb6e3cef5475b678b14985250
sauber <sauber@47f74d74-7b98-42f0-9597-0058cf2f60ed>
2005-04-22 08:44:16 +0000

    Make testentry behave more like insert.pl
    git-svn-id: svn+ssh://svn.code.sf.net/p/nagiosgraph/code@39 47f74d74-7b98-42f0-9597-0058cf2f60ed

--- trunk/nagiosgraph/testentry.pl
+++ trunk/nagiosgraph/testentry.pl
@@ -1,16 +1,30 @@
 #!/usr/bin/perl
 
-use Data::Dumper;
+# Modify this script to test map entries before inserting into real
+# map file. Run the script and check if the output is as expected.
 
+use strict;
+no strict "subs";
+use Data::Dumper;
+my @s;
+
+# Insert servicesdescr, output and perfdata here as it appears in log file.
+#
 $_ = <<DATA;
 servicedescr:ping
-output:PING OK - Packet loss = 0%, RTA = 0.31 ms
+output:Total RX Bytes: 4058.14 MB, Total TX Bytes: 2697.28 MB<br>Average Traffic: 3.57 kB/s (0.0%) in, 4.92 kB/s (0.0%) out| inUsage=0.0,85,98 outUsage=0.0,85,98
 perfdata:
 DATA
 
-/output:PING.*?(\d+)%.+?([.\d]+)\sms/
-and push @s, [ ping,
-               [ losspct, GAUGE, $1      ],
-               [ rta,     GAUGE, $2/1000 ] ];
+eval {
+
+# Insert here a map entry to parse the nagios plugin data above.
+#
+/output:.*Average Traffic.*?([.\d]+) kB.+?([.\d]+) kB/
+and push @s, [ rxbytes,
+               [ in,  GAUGE, $1 ],
+               [ out, GAUGE, $2 ] ];
+
+};
 
 print Data::Dumper->Dump([\@s], [qw(*s)]);
