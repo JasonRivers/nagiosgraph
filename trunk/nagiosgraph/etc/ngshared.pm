7a5fd07db545199c40f5e8748e4f2a80163c734a
abbrenner <abbrenner@47f74d74-7b98-42f0-9597-0058cf2f60ed>
2008-10-01 15:37:28 +0000

    fix test path and update version number for minor bug fix release
    git-svn-id: svn+ssh://svn.code.sf.net/p/nagiosgraph/code@165 47f74d74-7b98-42f0-9597-0058cf2f60ed

--- trunk/nagiosgraph/etc/ngshared.pm
+++ trunk/nagiosgraph/etc/ngshared.pm
@@ -66,7 +66,7 @@ use RRDs;
 
 use vars qw(%Config %Navmenu $colorsub $VERSION %Ctrans);
 $colorsub = -1;
-$VERSION = '1.1.0';
+$VERSION = '1.1.1';
 
 # Debug/logging support ########################################################
 my $prog = basename($0);
