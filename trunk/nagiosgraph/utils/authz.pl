f50a993bbe0945c24e07cf0dcd4236fad6b94cd3
abbrenner <abbrenner@47f74d74-7b98-42f0-9597-0058cf2f60ed>
2009-11-30 21:14:17 +0000

    post perlcritic
    git-svn-id: svn+ssh://svn.code.sf.net/p/nagiosgraph/code@231 47f74d74-7b98-42f0-9597-0058cf2f60ed

--- trunk/nagiosgraph/utils/authz.pl
+++ trunk/nagiosgraph/utils/authz.pl
@@ -1,8 +1,43 @@
 #!/usr/bin/perl
+# File:    $Id$
+# Author:  (c) Alan Brenner, Ithaka, 2009
+# License: OSI Artistic License
+#          http://www.opensource.org/licenses/artistic-license.php
 
 # The configuration file and ngshared.pm must be in this directory.
 use lib '/etc/nagios/nagiosgraph';
 # The configuration loader will look for nagiosgraph.conf in this directory.
+
+use strict;
+use warnings;
+use ngshared;
+use English qw(-no_match_vars);
+use Fcntl qw(:DEFAULT :flock);
+
+use vars qw($VERSION);
+$VERSION = '0.2';
+
+readconfig('write');
+
+my $lock = $Config{userdb} . '.lock';
+my %authz;
+
+sysopen DBLOCK, $lock, O_RDONLY | O_CREAT
+	or die "Cannot open lock file: $OS_ERROR\n";
+flock DBLOCK, LOCK_EX or die "cannot lock $lock: $OS_ERROR\n";
+tie %authz, $Config{dbfile}, $Config{userdb}, O_RDWR | O_CREAT ## no critic (ProhibitTies)
+	or die "cannot tie database $Config{dbfile}: $OS_ERROR\n";
+
+# Change this to suit your needs
+$authz{'server1'} = {'user1' => 1, 'user2' => 1};
+$authz{'server2'} = {'user1' => 1, 'user3' => 1};
+# end change this
+
+untie %authz;
+close DBLOCK or die "cannot close $lock: $OS_ERROR\n";
+unlink $lock;
+
+__END__
 
 =head1 NAME
 
@@ -21,11 +56,23 @@ that are allowed to see that host.
 Copy this file into a convinient directory (/usr/local/bin, for example) and
 modify line 4 to point to the directory with ngshared.pm.
 
-=head1 AUTHOR
+=head1 CONFIGURATION
 
-Alan Brenner - alan.brenner@ithaka.org, original author.
+=head1 USAGE
 
-=head1 BUGS
+=head1 REQUIRED ARGUMENTS
+
+=head1 OPTIONS
+
+=head1 EXIT STATUS
+
+=head1 DIAGNOSTICS
+
+=head1 DEPENDENCIES
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
 
 Undoubtedly there are some in here. I (Alan Brenner) have endevored to keep this
 simple and tested.
@@ -34,9 +81,13 @@ simple and tested.
 
 B<ngshared.pm>
 
-=head1 COPYRIGHT
+=head1 AUTHOR
 
-Copyright (C) 2009 Ithaka Harbors, Inc.
+Alan Brenner - alan.brenner@ithaka.org, original author.
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2009 Andrew W. Mellon Foundation
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the OSI Artistic License see:
@@ -45,30 +96,3 @@ http://www.opensource.org/licenses/artistic-license-2.0.php
 This program is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 PARTICULAR PURPOSE.
-
-=cut
-use strict;
-
-use ngshared;
-
-use Fcntl qw(:DEFAULT :flock);
-
-readconfig('write');
-
-my $lock = $Config{userdb} . + '.lock';
-my $authz;
-
-sysopen(DBLOCK, $lock, O_RDONLY | O_CREAT)
-	or die "Cannot open lock file: $!\n";
-flock(DBLOCK) or die "Cannot lock $lock: $!\n";
-tie(%authz, $Config{dbfile}, $Config{userdb}, O_RDWR | O_CREAT)
-	or die "Cannot tie database $Config{dbfile}: $!\n";
-
-# Change this to suit your needs
-$authz{'server1'} = {'user1' => 1, 'user2' => 1};
-$authz{'server2'} = {'user1' => 1, 'user3' => 1};
-# end change this
-
-untie %authz;
-close DBLOCK;
-unlink $lock;
