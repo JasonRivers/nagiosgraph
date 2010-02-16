ebee6d299fd78c1b92dd9e7f750d81f641ee0e3d
mwall <mwall@47f74d74-7b98-42f0-9597-0058cf2f60ed>
2010-02-16 15:45:21 +0000

    keep all javascript in single file (better opportunity for caching).  more README/INSTALL cleanup to help first-time users.
    git-svn-id: svn+ssh://svn.code.sf.net/p/nagiosgraph/code@288 47f74d74-7b98-42f0-9597-0058cf2f60ed

--- trunk/nagiosgraph/share/nagiosgraph.js
+++ trunk/nagiosgraph/share/nagiosgraph.js
@@ -9,6 +9,47 @@
 
 var PNAME = [ 'day', 'week', 'month', 'quarter', 'year' ];
 
+// show/hide a graph popup window (for mouseovers)
+var ngpopup;
+function showGraphPopup(elem) {
+  if(!elem || !elem.rel) return;
+  if(ngpopup == null) {
+    ngpopup = document.createElement('div');
+    ngpopup.style.position = 'absolute';
+    ngpopup.style.padding = '3';
+    ngpopup.style.background = '#dddddd';
+    ngpopup.style.border = '1px solid #777777';
+    ngpopup.style.filter='alpha(opacity=90)';
+    ngpopup.style.opacity='0.90';
+    document.body.appendChild(ngpopup);
+  }
+  var html = "<div class='graphPopup'>";
+  html += "<img src='" + elem.rel + "' alt='graph data'>";
+  html += "</div>";
+  ngpopup.innerHTML = html;
+  var coord = findPos(elem);
+  ngpopup.style.left = coord[0] + 20;
+  ngpopup.style.top = coord[1] + 16;
+  ngpopup.style.visibility = 'visible';
+}
+function hideGraphPopup() {
+  if(ngpopup != null) {
+    ngpopup.style.visibility = 'hidden';
+  }
+}
+function findPos(elem) {
+  var left = 0;
+  var top = 0;
+  if(elem.offsetParent) {
+    do {
+      left += elem.offsetLeft;
+      top += elem.offsetTop;
+    } while(elem=elem.offsetParent);
+  }
+  return [left, top];
+}
+
+
 // return the value (if any) for the indicated CGI key.  if we find the arg
 // but it has no value, return empty string.  if no arg, then return undefined.
 function getCGIValue(key) {
